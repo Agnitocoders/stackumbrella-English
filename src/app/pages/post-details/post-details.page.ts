2fd743dea7109685772fd3a8aa356824a0629798
nitesh8871 <nitesh@agnitotechnologies.com>
2023-03-16 15:05:53 +0530

    16-03-2023

--- src/app/pages/post-details/post-details.page.ts
+++ src/app/pages/post-details/post-details.page.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
-import { IonContent } from '@ionic/angular';
+import { IonContent, Platform } from '@ionic/angular';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MenuController } from '@ionic/angular';
 import { DataService } from 'src/app/services/data.service';
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 import { Router } from '@angular/router';
-// import { ifram}
+import { Admob, AdmobOptions } from '@awesome-cordova-plugins/admob';
 declare var window: any;
 @Component({
   selector: 'app-post-details',
@@ -21,20 +21,108 @@ export class PostDetailsPage implements OnInit {
   viewNow: boolean = false
   @ViewChild(IonContent)
   content!: IonContent;
+
+  admobOptions: AdmobOptions = {
+    bannerAdId: 'ca-app-pub-4957358923271966/8781967401',
+    interstitialAdId: 'ca-app-pub-4957358923271966/4312788095',
+    rewardedAdId: 'ca-app-pub-4957358923271966/4086722074',
+    isTesting: true,
+    autoShowBanner: false,
+    autoShowInterstitial: false,
+    autoShowRewarded: false,
+    adSize: Admob.AD_SIZE.BANNER
+  };
+
   constructor(
     public dataService: DataService,
     public menuCtr: MenuController,
     public domSanitizer: DomSanitizer,
     private socialSharing: SocialSharing,
     public router: Router,
+    // private admob: Admob,
+    private platform: Platform
   ) {
+    this.platform.ready().then(() => {
+      Admob.setOptions(this.admobOptions)
+        .then(() => console.log('Admob options have been successfully set'))
+        .catch((err: any) => console.error('Error setting admob options:', err));
+    })
 
+    // (Optionally) Load banner ad, in order to have it ready to show
+    Admob.createBannerView()
+      .then(() => console.log('Banner ad loaded'))
+      .catch(err => console.error('Error loading banner ad:', err));
+
+
+    // Show banner ad (createBannerView must be called before and onAdLoaded() event raised)
+    Admob.onAdLoaded().subscribe((ad) => {
+      if (ad.adType === Admob.AD_TYPE.BANNER) {
+        Admob.showBannerAd()
+          .then(() => console.log('Banner ad shown'))
+          .catch(err => console.error('Error showing banner ad:', err));
+      }
+    });
+
+
+    // On Ad loaded event
+    Admob.onAdLoaded().subscribe((ad) => {
+      if (ad.adType === Admob.AD_TYPE.BANNER) {
+        console.log('Banner ad is loaded');
+        Admob.showBannerAd();
+      } else if (ad.adType === Admob.AD_TYPE.INTERSTITIAL) {
+        console.log('Interstitial ad is loaded');
+        Admob.showInterstitialAd();
+      } else if (ad.adType === Admob.AD_TYPE.REWARDED) {
+        console.log('Rewarded ad is loaded');
+        Admob.showRewardedAd();
+      }
+    });
+
+
+    // On ad failed to load
+    Admob.onAdFailedToLoad().subscribe(err => console.log('Error loading ad:', err));
+
+
+
+    // On interstitial ad opened
+    Admob.onAdOpened().subscribe(() => console.log('Interstitial ad opened'));
+
+
+
+    // On interstitial ad closed
+    Admob.onAdClosed().subscribe(() => console.log('Interstitial ad closed'));
+
+
+
+    // On ad clicked and left application
+    Admob.onAdLeftApplication().subscribe(() => console.log('Ad lefted application'));
+
+
+
+    // On user ad rewarded
+    Admob.onRewardedAd().subscribe(() => console.log('The user has been rewarded'));
+
+
+
+    // On rewarded ad video started
+    Admob.onRewardedAdVideoStarted().subscribe(() => console.log('Rewarded ad vieo started'));
+
+
+
+    // On rewarded ad video completed
+    Admob.onRewardedAdVideoCompleted().subscribe(() => console.log('Rewarded ad video completed'));
   }
 
   ngOnInit() {
     this.menuCtr.close();
     // window.instgrm.Embeds.process()
+
+
+
+
   }
+
+
 
   ionViewWillEnter() {
     console.log('run by child')
